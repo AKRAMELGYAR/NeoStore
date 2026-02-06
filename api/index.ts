23d0bb047f68bb333be63931da2b49082afa58ab
Akram Elgyar <125706128+AKRAMELGYAR@users.noreply.github.com>
2026-02-06 03:18:09 +0200

    fix deploy bugs

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,13 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import { createApp } from '../src/main';
 
 let cachedServer: any;
 
-export default async function handler(req, res) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
     if (!cachedServer) {
         cachedServer = await createApp();
     }
 
+    // Express/NestJS will handle the request and response
     return cachedServer(req, res);
 }
