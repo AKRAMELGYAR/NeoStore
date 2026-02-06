771d8cb962cbb972f69ee2c256fecbe8065871ec
Akram Elgyar <125706128+AKRAMELGYAR@users.noreply.github.com>
2026-02-06 02:53:23 +0200

    make nestjs work on vercel serverless

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+import { createApp } from '../src/main';
+
+let cachedServer: any;
+
+export default async function handler(req, res) {
+    if (!cachedServer) {
+        cachedServer = await createApp();
+    }
+
+    return cachedServer(req, res);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,39 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, {
+//     cors: true,
+//     rawBody: true
+//   }
+//   );
+//   await app.listen(process.env.Port ?? 5000)
+//   console.log(`Server is running on port ${process.env.Port ?? 5000}`);
+// }
+// bootstrap();
+
+
+////////////////////////////////////////
+//support vercel deployment by converting it to serverless
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
+const server = express();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-    rawBody: true
-  }
+export async function createApp() {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+    {
+      cors: true,
+      rawBody: true,
+    },
   );
-  await app.listen(process.env.Port ?? 5000)
-  console.log(`Server is running on port ${process.env.Port ?? 5000}`);
+
+  await app.init();
+  return server;
 }
-bootstrap();
